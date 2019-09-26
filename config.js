50e543da6fd1fd515a0a4da14b395d05a2148f03
Admin <admin@Admins-MacBook-7.local>
2019-09-26 10:18:52 -0500

    Fixed token expiry loop issue

--- config.js
+++ config.js
@@ -6,4 +6,4 @@ exports.TEST_DATABASE_URL = process.env.TEST_DATABASE_URL ||
                       'mongodb://127.0.0.1/test-character-app';
 exports.PORT = process.env.PORT || 8080;
 exports.JWT_SECRET = process.env.JWT_SECRET;
-exports.JWT_EXPIRY = process.env.JWT_EXPIRY || '7d';
+exports.JWT_EXPIRY = process.env.JWT_EXPIRY || '3d';
